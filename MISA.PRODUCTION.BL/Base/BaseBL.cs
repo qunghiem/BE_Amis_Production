da1e5f453262e8c74309320fb3a000c56b5ec70b
Quân Nghiêm <nghiemxuanquan2003@gmail.com>
2026-05-10 10:13:45 +0700

    first commit

---
+++ MISA.PRODUCTION.BL/Base/BaseBL.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MISA.PRODUCTION.BL.Base
+{
+    internal class BaseBL
+    {
+    }
+}
